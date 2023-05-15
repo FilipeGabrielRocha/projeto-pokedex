2432e893df715ae0f342c98ed04dd9752e288a77
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-05-15 10:07:56 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
 document.querySelector("#botao-alterar-tema").addEventListener("click", () => {
   if (document.querySelector("body").classList.contains("modo-noturno")) {
-    tirarClasse("body", "modo-noturno")
-    mudarIcone(".imagem-botao", "src", "src/images/sun.png")
+    tirarClasse("body", "modo-noturno");
+    mudarIcone(".imagem-botao", "src", "src/images/sun.png");
   } else {
-    colocarClasse("body", "modo-noturno")
-    mudarIcone(".imagem-botao", "src", "src/images/moon.png")
+    colocarClasse("body", "modo-noturno");
+    mudarIcone(".imagem-botao", "src", "src/images/moon.png");
   }
 });
 
 function tirarClasse(item, classe) {
-    document.querySelector(item).classList.remove(classe);
+  document.querySelector(item).classList.remove(classe);
 }
 
 function colocarClasse(item, classe) {
-    document.querySelector(item).classList.add(classe);
+  document.querySelector(item).classList.add(classe);
 }
 
 function mudarIcone(item, atributo, caminhoImagem) {
-    document.querySelector(item).setAttribute(atributo, caminhoImagem);
+  document.querySelector(item).setAttribute(atributo, caminhoImagem);
 }
